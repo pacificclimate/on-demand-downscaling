d2ea654bdd9289246be59718f9ddd398d2477f94
Quintin <quintins@uvic.ca>
2026-03-13 12:22:38 -0700

    Add browser tab title and favicon

--- panel_app/panel_app.py
+++ panel_app/panel_app.py
@@ -4,4 +4,6 @@ from panel_UI.state import get_main_pane, get_help_pane, render
 pn.extension("ipywidgets")
 
 render()
-pn.Column(get_main_pane(), pn.layout.HSpacer(), get_help_pane()).servable()
+pn.Column(get_main_pane(), pn.layout.HSpacer(), get_help_pane()).servable(
+    title="PCIC On-Demand Downscaling Application"
+)
